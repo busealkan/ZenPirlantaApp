edefec5e261363d561bbbf682929fec125bb0b64
Buse Alkan <60299020+busealkan@users.noreply.github.com>
2021-06-17 22:13:28 +0300

    Update Constants.kt

--- app/src/main/java/com/h5190059/zenpirlanta/util/Constants.kt
+++ app/src/main/java/com/h5190059/zenpirlanta/util/Constants.kt
@@ -1,12 +1,12 @@
 package com.h5190059.zenpirlanta.util
 
 object Constants {
-     val  TIMER_INTERVAL = 1000
+          val  TIMER_INTERVAL = 1000
      val  TIMER_MILLIS_IN_FUTURE= 3000
 
-     val BASE_URL="https://raw.githubusercontent.com/busealkan/FinalDeneme/main/"
-     const val JSON_KATEGORI_VE_URUNLER_DOSYA_ADI="kategorilerVeUrunler.json"
-     const val JSON_USER_DOSYA_ADI="users.json"
+     val BASE_URL="https://raw.githubusercontent.com/busealkan/ZenPirlantaApp/master/data/"
+     const val JSON_KATEGORI_VE_URUNLER_DOSYA_ADI="KategorilerVeUrunler.json"
+     const val JSON_USER_DOSYA_ADI="Users.json"
 
 
      val TASINAN_KULLANICI_AD="ad"
@@ -19,7 +19,9 @@ object Constants {
      val LOCALE = "TR-tr"
 
      val SPAN_COUNT=2
-     val btnFavoriBg1=1
-     val btnFavoriBg2=2
+     val BTN_FAVORI_BG1=1
+     val BTN_FAVORI_BG2=2
+     val ARTAN_SIRALAMA=0
+     val AZALAN_SIRALAMA=1
 
 }
